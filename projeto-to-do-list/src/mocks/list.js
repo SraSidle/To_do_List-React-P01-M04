25dacdb9662ab1c9a8eca9377a1909630c55df85
Bianca Aguiar <biancaaguiar2021@gmail.com>
2022-06-28 22:40:31 -0300

    lista de tarefas em memória

---
+++ projeto-to-do-list/src/mocks/list.js
@@ -0,0 +1,16 @@
+export const tasks = [{
+    toDo: "Fazer o projeto",
+    index: 1,
+    value: true
+}, 
+{
+    toDo: "Consumir a API",
+    index: 2,
+    value: true
+},
+{
+    toDo: "Estilizar",
+    index: 3,
+    value: true
+},
+]
