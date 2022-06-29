a0631e289c7de898f9b48fea21873a07223313b1
Bianca Aguiar <biancaaguiar2021@gmail.com>
2022-06-29 19:34:06 -0300

    retirando os erros temporariamente

--- projeto-to-do-list/src/components/Items/Item.jsx
+++ projeto-to-do-list/src/components/Items/Item.jsx
@@ -1,23 +1,20 @@
 import "./Item.css";
 
 function Item() {
-  return (
-    
-    <div className="item">
-      <div className="task--list--item"  key={`task--list-${index}`}>
+  return (   
+      <div className="task--list--item"  key={`task--list-${"index" }`}>
         <div className="check--item">
           <input type="checkbox" />
-          {task.toDo}
+          {"task.toDo"}
         </div>
         <div className="icons-edit-delete">
           <i className="bi bi-pencil-square"></i>
           <i className="bi bi-trash"></i>
         </div>
       </div>
-    </div>
   );
 }
 
 export default Item;
 
-// Na linha 6/ o key vai ficar no tasklist, rever a aula 
+// Na linha 6/ o key vai ficar no tasklist, rever a aula / na key tem o "index" no template e na 9 trm um task.toDo
--- projeto-to-do-list/src/components/TaskList/TaskList.jsx
+++ projeto-to-do-list/src/components/TaskList/TaskList.jsx
@@ -1,27 +1,20 @@
 import "./TaskList.css";
 import { tasks } from "../../mocks/list";
 import Search from "../Search/Search";
-//import Item from "../Items/Item";
+import Item from "../Items/Item";
 
 function TaskList() {
   return (
     <div className="task--list">
       {" "}
       <Search />
-      {tasks.map((task, index) => (
-        <div className="task--list--item" key={`task--list-${index}`}>
-          <div className="check--item">
-            <input type="checkbox" />
-            {task.toDo}
-          </div>
-          <div className="icons-edit-delete">
-            <i className="bi bi-pencil-square"></i>
-            <i className="bi bi-trash"></i>
-          </div>
-        </div>
-      ))}
+      {tasks.map((task, index) => (      
+         <Item />
+))}
     </div>
   );
 }
 
 export default TaskList;
+
+// linha 11 erro pq index e task nao esta disponivel
