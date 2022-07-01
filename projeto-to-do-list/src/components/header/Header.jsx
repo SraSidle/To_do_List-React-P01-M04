a6ba6a11bdb9acfef97bef8c9acacc99cd27057f
Bianca Aguiar <biancaaguiar2021@gmail.com>
2022-07-01 19:41:30 -0300

    add

--- projeto-to-do-list/src/components/header/Header.jsx
+++ projeto-to-do-list/src/components/header/Header.jsx
@@ -1,14 +1,23 @@
 import "./header.css";
+import { useState } from "react";
 
 function Header() {
+  const [task, setTask] = useState("");
   return (
     <header>
-      <h1><i className="bi bi-list-check"></i>To do List</h1>
+      <h1>
+        <i className="bi bi-list-check"></i>To do List
+      </h1>
       <div>
         <button className="bi">
           <i className="bi bi-plus-lg"></i>
         </button>
-        <input type="text" placeholder="Adicione uma nova tarefa" />
+        <input
+          type="text"
+          placeholder="Adicione uma nova tarefa"
+          value={task}
+          onChange={(ev) => setTask(ev.target.value)}
+        />
       </div>
     </header>
   );
