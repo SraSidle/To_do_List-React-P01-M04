6243374d52a6359ef5829672cf4e5c2c801f1f91
Bianca Aguiar <biancaaguiar2021@gmail.com>
2022-07-02 23:10:29 -0300

    colocacao da funcao create

--- projeto-to-do-list/src/components/header/Header.jsx
+++ projeto-to-do-list/src/components/header/Header.jsx
@@ -1,22 +1,44 @@
+import React,{useState, useEffect }  from 'react'
+import { TasksServices } from '../../services/TasksServices';
+import {Api} from '../../helpers/Api'
 import "./header.css";
-import { useState } from "react";
 
 function Header() {
-  const [task, setTask] = useState("");
+  const [newTask, setnewTask] = useState();
+
+  useEffect(()=>{
+     TasksServices.getlista().then(setnewTask)
+  }, [])
+
+  const task = newTask
+
+  async function create(task){
+    const response = await fetch(Api.baseURL, {
+      method: "POST",
+      headers: {
+        'Content-type': 'application/json',
+      },
+      mode: 'cors',
+      body: JSON.stringify(task),
+    })
+    const newTask = await response.json()
+    setnewTask([newTask])
+  }
+
   return (
     <header>
       <h1>
         <i className="bi bi-list-check"></i>To do List
       </h1>
       <div>
-        <button className="bi">
+        <button type="button" className="bi" onClick={() => create(task)}>
           <i className="bi bi-plus-lg"></i>
         </button>
         <input
           type="text"
           placeholder="Adicione uma nova tarefa"
-          value={task}
-          onChange={(ev) => setTask(ev.target.value)}
+         // value={task}
+         //onChange={(ev) => setnewTask(ev.target.value)}
         />
       </div>
     </header>
