8f592ff8628c27e888035749566e3b38781e2369
Bianca Aguiar <biancaaguiar2021@gmail.com>
2022-07-02 23:11:04 -0300

    acrescimo do findById

--- projeto-to-do-list/src/components/Search/Search.jsx
+++ projeto-to-do-list/src/components/Search/Search.jsx
@@ -1,6 +1,15 @@
+import React,{useState, useEffect} from 'react'
+import { TasksServices } from '../../services/TasksServices';
 import "./search.css";
 
 function SearchTask() {
+
+  const [found, setFound] = useState([]);
+
+  useEffect(()=>{
+     TasksServices.getById().then(setFound)
+  }, [])
+
   return (
     <div className="align-end">
       <div className="search--task">
