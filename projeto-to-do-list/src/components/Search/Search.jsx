49e306959d50f0b974e8bdd4b20a3941407cadf6
Bianca Aguiar <biancaaguiar2021@gmail.com>
2022-07-04 20:50:07 -0300

    mesma

--- projeto-to-do-list/src/components/Search/Search.jsx
+++ projeto-to-do-list/src/components/Search/Search.jsx
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from "react";
 import "./search.css";
-import { tasks } from "../../mocks/list";
+//import { tasks } from "../../mocks/list";
 //import { Api } from "../../helpers/Api";
+// import Item from "../Item/Item";
+// import { render } from "@testing-library/react";
 import { TasksServices } from "../../services/TasksServices";
 
 function SearchTask() {
@@ -10,32 +12,38 @@ function SearchTask() {
   const searchClick = async () => {
     const task_id_search = document.getElementById("search--input").value;
     const response = await TasksServices.getById(task_id_search);
-    setFound(response)
- };
+    console.log("response:", response)
+    console.log("found:", found)
+    setFound(response);
+    
+  };
 
   useEffect(() => {
-    searchClick()
+    searchClick(); 
   }, []);
 
   return (
-    <div className="align-end">
-      <div className="search--task">
-        <input
-          id="search--input"
-          className="search"
-          type="text"
-          placeholder="Pesquise uma Tarefa"
-        />
-        <button
-          type="button"
-          onClick={() => {
-            searchClick();
-          }}
-        >
-          <i className="bi bi-search"></i>
-        </button>
-      </div>
-    </div>
+    <>
+      {" "}
+      <div className="align-end">
+        <div className="search--task">
+          <input
+            id="search--input"
+            className="search"
+            type="text"
+            placeholder="Pesquise uma Tarefa"
+          />
+          <button
+            type="button"
+            onClick={() => {
+              searchClick();
+            }}
+          >
+            <i className="bi bi-search"></i>
+          </button>
+        </div>
+      </div> 
+    </>
   );
 }
 
