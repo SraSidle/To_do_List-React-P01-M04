5721e23b1cbc52b9ca8491854c8d0caa4c220679
Bianca Aguiar <biancaaguiar2021@gmail.com>
2022-07-04 23:01:49 -0300

    criacao e inicio da estilizacao mcreate

---
+++ projeto-to-do-list/src/components/ModalCreate/Mcreate.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import "./Mcreate.css"
+
+function Mcreate(){
+    return(
+        <div className="Modal--create--overlay" data-anime="rigth">
+            <div className="Modal--create">
+                <div className="end">X</div>
+            <p>Tarefa criada com sucesso!</p>
+        </div> 
+        </div>
+       
+    )
+}
+
+export default Mcreate;
