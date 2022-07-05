d59e3d83104512b7118cd9aef6ad1b05bfcf8b77
Bianca Aguiar <biancaaguiar2021@gmail.com>
2022-07-04 21:21:54 -0300

    retirada da rota create e do console.log

--- projeto-to-do-list/src/services/TasksServices.js
+++ projeto-to-do-list/src/services/TasksServices.js
@@ -1,13 +1,12 @@
 import {Api} from '../helpers/Api'
 
 const parseResponse = (response) => response.json();
-console.log(parseResponse)
 
 export const TasksServices = {
     getlista: () => 
      fetch((Api.baseURL + "/tasks"), {method: "GET"}).then(parseResponse),  
     getById: (id) =>
-     fetch((Api.baseURL + "/tasks/" +`${id}`), {method: "GET"}).then(parseResponse),
-    create: (task) =>
-     fetch(Api.baseURL + "/tasks/create-" + task, {method: "POST"}).then(parseResponse),
+     fetch((Api.baseURL + `/tasks/${id}`), {method: "GET"}).then(parseResponse),
+    // create: (task) =>
+    //  fetch(Api.baseURL + "/tasks/create-" + task, {method: "POST"}).then(parseResponse),
 }
