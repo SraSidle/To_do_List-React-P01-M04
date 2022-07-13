89c393d141c59963a98d1e6ad3b73800578e6d7c
Bianca Aguiar <biancaaguiar2021@gmail.com>
2022-07-12 22:46:04 -0300

    novos parametros

--- projeto-to-do-list/src/components/TaskList/TaskList.jsx
+++ projeto-to-do-list/src/components/TaskList/TaskList.jsx
@@ -5,7 +5,7 @@ import ModalDelete from "../ModalDelete/ModalDelete";
 import SearchTask from "../Search/Search";
 import Item from "../Item/Item";
 
-function TaskList({updateList, updateTask, deleteTask, deletedTask }) {
+function TaskList({ updateList, updateTask, deleteTask, deletedTask }) {
   //A chave é colocada para desconstruir a sintaxe props.updateList para se tornar apenas updateList
   const [tasks, setTasks] = useState([]);
 
@@ -15,15 +15,16 @@ function TaskList({updateList, updateTask, deleteTask, deletedTask }) {
 
   return (
     <div className="task--list">
-      <SearchTask setTasks = {setTasks} />
-      {tasks && tasks.map((task, index) => (
-        <Item 
-          key={`task--list-${index}`}
-          task={task} 
-          // onModalDelete={setDelTaskModal(true)}
-          // onModalEdit={}
+      <SearchTask setTasks={setTasks} />
+      {tasks &&
+        tasks.map((task, index) => (
+          <Item
+            key={`task--list-${index}`}
+            task={task}
+            // onModalDelete={setDelTaskModal(true)}
+            // onModalEdit={}
           />
-      ))}
+        ))}
     </div>
   );
 }
@@ -31,7 +32,7 @@ function TaskList({updateList, updateTask, deleteTask, deletedTask }) {
 export default TaskList;
 
 //Toda vez que o updateList mudar o useEffect será chamado e atualizará a lista de tarefas.
-  // Existem 3 formas de utilizar o useEffect("Ciclos de vida") //quando o componente é atualizado, renderizado e desmontado
-  // 1- Se o UE não tem array de dependências: Atualiza sempre de acordo com seus ciclos de vida
-  // 2- Array de dep. vazio []: vai executar só quando for renderizado.
-  // 3- Com array de dependências: Quando renderizado e quando algum estado no Array de dependencias é atualizado
+// Existem 3 formas de utilizar o useEffect("Ciclos de vida") //quando o componente é atualizado, renderizado e desmontado
+// 1- Se o UE não tem array de dependências: Atualiza sempre de acordo com seus ciclos de vida
+// 2- Array de dep. vazio []: vai executar só quando for renderizado.
+// 3- Com array de dependências: Quando renderizado e quando algum estado no Array de dependencias é atualizado
