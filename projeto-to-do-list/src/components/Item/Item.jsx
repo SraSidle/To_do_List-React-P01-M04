0d444a4bf50ce194098dd3dfe965988383c2303e
Bianca Aguiar <biancaaguiar2021@gmail.com>
2022-07-15 23:06:18 -0300

    Modal alterado no click e não na function

--- projeto-to-do-list/src/components/Item/Item.jsx
+++ projeto-to-do-list/src/components/Item/Item.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import { useState } from "react";
 import ModalDelete from "../ModalDelete/ModalDelete";
+import ModalEdit from "../ModalEdit/ModalEdit"
 import "./Item.css";
 
-function Item({ task, onModalEdit, taskToDelete}) {
+function Item({ task, taskToDelete}) {
   const [delTaskModal, setDelTaskModal] = useState(false);
 
-  const booleanModal = () => {
-    setDelTaskModal(true);
-  };
+  const [editTaskModal, setEditTaskModal] = useState(false)
 
   return (
     <div className="task--list--item" key={`task--list-${"index"}`}>
@@ -19,15 +18,22 @@ function Item({ task, onModalEdit, taskToDelete}) {
           onDeleteTask={taskToDelete}
         />
       )}
+      {editTaskModal && (
+        <ModalEdit 
+        taskTitle={task.title}
+        taskId={task.id}
+        closeModal={() => setEditTaskModal(false)}
+        />
+      )}
       <div className="check--item">
         <input className="checkbox" type="checkbox" />
         <p className="Task-name">{task.title}</p>
       </div>
       <div className="icons-edit-delete">
-        <button type="button" className="" onClick={() => onModalEdit(task.id)}>
+        <button type="button" className="" onClick={() => setEditTaskModal(true)}>
           <i className="bi bi-pencil-square"></i>
         </button>
-        <button type="button" className="" onClick={() => booleanModal(true)}>
+        <button type="button" className="" onClick={() => setDelTaskModal(true)}>
           <i className="bi bi-trash"></i>
         </button>
       </div>
