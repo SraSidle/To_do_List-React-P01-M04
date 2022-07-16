b47cb77e38ab0c9ebfdb87b36d1b00510b5350b8
Bianca Aguiar <biancaaguiar2021@gmail.com>
2022-07-15 23:07:16 -0300

    criacao e configuracao do Modaledit

--- projeto-to-do-list/src/components/ModalEdit/ModalEdit.jsx
+++ projeto-to-do-list/src/components/ModalEdit/ModalEdit.jsx
@@ -1,1 +1,35 @@
-import "./ModalEdit.css"
+import React from "react";
+import { useState } from "react";
+import "./ModalEdit.css";
+import Modal from "../Modal/Modal";
+import { TasksServices } from "../../services/TasksServices";
+
+function ModalEdit({ closeModal, taskTitle, taskId}) {
+  const [taskToUpdate, setTaskToUpdate] = useState({title:""});
+  const [taskUpdated, setTaskUpdated] = useState({title: "", id: "", checked: false,})
+
+  const editTask = async (tasks,taskupdate) => {
+    tasks = taskId;
+    taskupdate = taskToUpdate
+    const response = await TasksServices.updateById(tasks, taskupdate);
+    closeModal();
+    // setTaskUpdated(response)
+  };
+
+  return (
+    <Modal closeModal={closeModal}>
+      <form className="form__edit"
+      onSubmit={(event) => {event.preventDefault();
+      editTask()}}>
+          <input
+           type="text"
+            placeholder={taskTitle}
+            onChange={(event) => setTaskToUpdate(event.target.value)}
+          />
+        <button type="button" onClick={() => editTask()}>Atualizar</button>
+      </form>
+    </Modal>
+  );
+}
+
+export default ModalEdit;
