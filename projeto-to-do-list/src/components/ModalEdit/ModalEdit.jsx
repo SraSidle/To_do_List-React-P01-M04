489b6a0ee89e54c5d4a6795fee9afe9a103fc626
Bianca Aguiar <biancaaguiar2021@gmail.com>
2022-07-18 21:02:54 -0300

    configuracao edit

--- projeto-to-do-list/src/components/ModalEdit/ModalEdit.jsx
+++ projeto-to-do-list/src/components/ModalEdit/ModalEdit.jsx
@@ -4,29 +4,47 @@ import "./ModalEdit.css";
 import Modal from "../Modal/Modal";
 import { TasksServices } from "../../services/TasksServices";
 
-function ModalEdit({ closeModal, taskTitle, taskId}) {
-  const [taskToUpdate, setTaskToUpdate] = useState({title:""});
-  const [taskUpdated, setTaskUpdated] = useState({title: "", id: "", checked: false,})
+function ModalEdit({ closeModal, taskTitle, taskId }) {
+  // const [taskToUpdate, setTaskToUpdate] = useState({title:""});
 
-  const editTask = async (tasks,taskupdate) => {
-    tasks = taskId;
-    taskupdate = taskToUpdate
+  const [taskUpdated, setTaskUpdated] = useState({
+    title: taskTitle,
+    id: taskId,
+    checked: false,
+  });
+
+  const editTask = async (tasks, taskupdate) => {
+    // tasks = taskId;
+    delete taskupdate.id;
     const response = await TasksServices.updateById(tasks, taskupdate);
     closeModal();
+    // taskupdate = taskToUpdate
     // setTaskUpdated(response)
   };
 
   return (
     <Modal closeModal={closeModal}>
-      <form className="form__edit"
-      onSubmit={(event) => {event.preventDefault();
-      editTask()}}>
-          <input
-           type="text"
-            placeholder={taskTitle}
-            onChange={(event) => setTaskToUpdate(event.target.value)}
-          />
-        <button type="button" onClick={() => editTask()}>Atualizar</button>
+      <form
+        className="form__edit"
+        onSubmit={(event) => {
+          console.log(taskId);
+          event.preventDefault(); // senão vai tentar mandar os dados via navegador, muda a url e coloca as informaçoes, não manda pra API
+          editTask(taskUpdated.id, taskUpdated);
+        }}
+      >
+        <input
+          type="text"
+          placeholder={taskTitle}
+          value={taskUpdated.title} //
+          name="title" //add name e value
+          onChange={(event) =>
+            setTaskUpdated({ ...taskUpdated, title: event.target.value })
+          }
+        />
+        <button type="submit">
+          {/* //onClick={() => editTask()} */}
+          Atualizar
+        </button>
       </form>
     </Modal>
   );
