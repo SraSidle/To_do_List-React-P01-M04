902daef1626e6c2678bfcad4d5b48af697715b0b
Bianca Aguiar <biancaaguiar2021@gmail.com>
2022-07-18 22:52:49 -0300

    retirada parentese a mais

--- projeto-to-do-list/src/services/TasksServices.js
+++ projeto-to-do-list/src/services/TasksServices.js
@@ -11,14 +11,14 @@ export const TasksServices = {
   //  fetch(Api.baseURL + "/tasks/create-" + task, {method: "POST"}).then(parseResponse),
   updateById: (id, task) =>
     fetch(
-      (Api.baseURL + `/tasks/${id}`,
+    Api.baseURL + `/tasks/${id}`,
       {
         method: "PUT",
         body: JSON.stringify(task),
         mode: "cors",
         headers: { "Content-Type": "application/json" },
-      })
-    ).then(parseResponse),
+      }
+    ).then(parseResponse), // tinha um parêntese a mais
   deleteById: (id) =>
     fetch(Api.baseURL + `/tasks/${id}`, { method: "DELETE" }).then(
       parseResponse
